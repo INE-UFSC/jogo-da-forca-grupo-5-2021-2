7af27cea71ebc2bdf9fda70f46c49e42c9f98ae7
ctc <gabrieljacquesvilela@gmail.com>
2021-11-02 20:34:27 -0300

    Adição das funções restantes do jogo a atualização do README

--- README.md
+++ README.md
@@ -5,12 +5,11 @@
 ##### Funcionalidades:
 
 **Menu:**
-    - Escolher o tema para o jogo;
-    - Sair do jogo.
+    - Escolher o tema para o jogo;<br/>
+    - Sair do jogo.<br/>
 
 **Jogo:**
-    - Seleciona aleatóriamente uma palavra no tema escolhido;
-    - Checa os chutes do jogador para evitar inputs inválidos;
-    - Procura pelo chute na palavra e imprime a forca e o progresso da palavra;
+    - Seleciona aleatóriamente uma palavra no tema escolhido;<br/>
+    - Checa os chutes do jogador para evitar inputs inválidos;<br/>
+    - Procura pelo chute na palavra e imprime a forca e o progresso da palavra;<br/>
     - Função para testar se a letra inputada tem na palavra.
-    
--- game.py
+++ game.py
@@ -7,7 +7,6 @@ count = 0
 end_count = 0
 again = True
 
-
 def validate_input(guessed, word):
     global count
     global tested
@@ -17,7 +16,7 @@ def validate_input(guessed, word):
     elif guessed not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
         print('Entrada invalida! Digite uma letra')
         guess()
-
+    
     elif guessed in tested:
         print('Entrada invalida! Você já digitou essa letra...')
         guess()
@@ -31,7 +30,6 @@ def validate_input(guessed, word):
         print(f"Letras testadas: {tested}")
         print(f'Vidas: {6 - count}')
 
-
 def validate_guess(word):
     global end_count
     end_count = 0
@@ -43,7 +41,6 @@ def validate_guess(word):
             print('_ ', end=' ')
     print()
     print()
-
 
 def hangman():
     global count
@@ -84,6 +81,80 @@ def hangman():
         print("|     O")
         print("|    /|\ ")
         print("|    / \ ")
-
+    
     print('|\n|__')
     print()
+        
+def guess():
+    guess = input("Adivinhe uma letra:").strip().upper()
+    validate_input(guess, word)
+
+def end_game_message():
+    global word
+    global end_count
+    global count
+    global tested
+    if (end_count == len(word)):
+        print(f'Jogador, você acertou a palavra secreta: {word}.\nParabéns, você venceu o jogo!\n')
+    elif (count == 6):
+        print(f'Jogador, você perdeu!\nA palavara correta era: {word}\n')
+    tested = []
+    count = 0
+    end_count = 0
+    menu()
+
+def menu():
+
+    print('Seja bem vindo jogador!')
+    sleep(1)
+    while True:
+        menu = input(f'Por favor, escolha uma das classes de palavras para jogar:\n'
+                    f'[1] - Frutas\n'
+                    f'[2] - Animal\n'
+                    f'[3] - Objeto\n'
+                    f'[4] - Cor\n'
+                    f'[5] - Time de Futebol\n'
+                    f'[6] - Aleatorio (inclui todas as classes anteriores);\n'
+                    f'[7] - Sair do Jogo.\n'
+                     'Insira aqui: ')
+        sleep(1)
+        if menu == '1':
+            word = random.choice(fruits)
+            break
+        elif menu == '2':
+            word = random.choice(animals)
+            break   
+        elif menu == '3':
+            word = random.choice(objects)
+            break
+        elif menu == '4':
+            word = random.choice(colors)
+            break 
+        elif menu == '5':
+            word = random.choice(soccer_teams)
+            break
+        elif menu == '6':
+            word = random.choice(random_list)
+            break
+        elif menu == '7':
+            quit()
+        else:
+            print('Por favor, selecione uma das opções válidas!')
+    return word
+
+word = menu()
+
+def play_again():
+    global word
+    while True:
+        play = input('Deseja continuar jogando? [sim/nao]\n'
+            'Resposta: ')
+        if play == 'sim' or 's':
+            word = menu()
+            break
+        elif play == 'nao' or 'n':
+            quit()
+        else:
+            print('Por favor, responda com "sim" ou "nao".')
+        return again
+        
