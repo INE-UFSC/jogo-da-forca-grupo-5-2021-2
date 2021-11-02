a196d817ef3f4e68627bbef33725ce28f38afd33
BonomoJoaoPaulo <jpabonomo@gmail.com>
2021-11-02 20:09:10 -0300

    Arquivo com as listas de palavras usadas na forca

---
+++ words.py
@@ -0,0 +1,36 @@
+
+fruits = ['MACA', 'BANANA', 'GOIABA', 'LARANJA', 'ABACAXI', 'JABUTICABA', 'KIWI', 'MANGA', 'MORANGO', 'UVA', 
+        'PERA', 'MARACUJA', 'LIMAO', 'MIRTILO', 'CEREJA', 'AMORA', 'MAMAO', 'MELAO', 'CUPUACU', 'JAMBO']
+
+animals = ['CAPIVARA', 'LOBO', 'URSO', 'TOUPEIRA', 'RATO', 'ABELHA', 'CACHORRO', 'GALO', 'GATO', 'ORNITORRINCO', 
+'BOI', 'RINOCERONTE', 'PREGUICA', 'MORCEGO', 'GOLFINHO', 'BALEIA', 'TUBARAO', 'MACACO', 'GALINHA', 'JAVALI']
+
+objects = ['LAPISEIRA', 'LIVRO', 'CADERNO', 'TECLADO', 'CELULAR', 'VASSOURA', 'CANUDO', 'PRATO', 'ENXADA', 
+'ISQUEIRO', 'VENTILADOR', 'PUNHAL', 'MASCARA', 'TAPETE', 'SABONETE', 'CADEIRA', 'GRAMPEADOR', 'MANGUEIRA', 
+'CARTEIRA', 'BOLSO']
+
+colors = ['ROSA', 'VERMELHO', 'ROXO', 'AMARELO', 'LARANJA', 'AZUL', 'PRETO', 'BRANCO', 'CINZA', 'VERDE', 
+'MARROM', 'LILAS', 'BEGE', 'INDIGO', 'CIANO', 'CREME', 'DOURADO', 'ESMERALDA', 'MAGENTA', 'SALMAO']
+
+soccer_teams = ['FIGUEIRENSE', 'AVAI', 'CHAPECOENSE', 'FLAMENGO', 'FLUMINENSE', 'VASCO', 'BOTAFOGO', 
+'INTERNACIONAL', 'GREMIO', 'JUVENTUDE', 'PALMEIRAS', 'SANTOS', 'CORINTHIANS', 'ATLETICO', 'CRUZEIRO', 
+'ATHLETICO', 'CORITIBA', 'BAHIA', 'SPORT', 'GOIAS']
+
+
+random_list = []
+
+for f in fruits:
+    random_list.append(f)
+
+for a in animals:
+    random_list.append(a)
+
+for o in objects:
+    random_list.append(o)
+
+for c in colors:
+    random_list.append(c)
+
+for t in soccer_teams:
+    random_list.append(t)
+
